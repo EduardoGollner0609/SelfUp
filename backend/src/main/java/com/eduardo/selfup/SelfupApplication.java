7d6aff4cbd8a5106fbff1443531a912d549906b6
Eduardo Sousa Gollner <eduardo.gollner@solutionsa.com.br>
2025-08-16 17:21:00 -0300

    Criando projeto spring/backend.

---
+++ backend/src/main/java/com/eduardo/selfup/SelfupApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.selfup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardo/selfup/SelfupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.selfup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
